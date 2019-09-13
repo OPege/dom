d03f82bb31e4d21b1924c14c6ea0b20049a2fce5
OPege <pgustalves@hotmail.com>
2019-09-13 11:50:40 -0300

    terminar dom

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let menuHamburguer = document.querySelector(".menu-hamburguer");
 let nav = document.querySelector("nav");
+let input = document.querySelector("input");
+let botao = document.querySelector("button");
+let h2 = document.querySelector("h2");
 
 function mostrarMenu(){
    // nav.style.display = "block";
@@ -15,7 +18,16 @@ function mostrarMenu(){
     // }
     //Jeito 3
     nav.classList.toggle("visivel");
+    menuHamburguer.classList.toggle("ativo");
+}
+
+function pegarNome(){
+    let nome = input.value;
+    h2.innerHTML = `Seja Bem-Vindo ${nome}`;
+    input.value = "";
+
 }
 
 menuHamburguer.onclick = 
 mostrarMenu;
+botao.onclick = pegarNome;
